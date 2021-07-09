0798bd5fc346b37146a4a529886b6b5b20340d39
James Hogan <james@albanarts.com>
2021-07-09 22:47:30 +0100

    setupViewerDefaults: Stop overriding threading model
    We shouldn't in principle need to force single threading, so lets drop
    the forcing of SingleThreading from setupViewerDefaults().
    
    Signed-off-by: James Hogan <james@albanarts.com>

--- src/osgXR.cpp
+++ src/osgXR.cpp
@@ -29,9 +29,6 @@ void osgXR::setupViewerDefaults(osgViewer::Viewer *viewer,
         xr->setValidationLayer(true);
         viewer->apply(xr);
 
-        // Force single threaded to make sure that no other thread can use the GL context
-        viewer->setThreadingModel(osgViewer::Viewer::SingleThreaded);
-
         OSG_WARN << "Setting up VR" << std::endl;
     }
 }
