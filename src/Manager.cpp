amalon/osgXR

commit 08efbe1d80a19ac23dcf31f61357618739bb21b3

Manager: Expose system name
Expose the OpenXR system name via a new osgXR::Manager::getSystemName().

Signed-off-by: James Hogan <james@albanarts.com>

src/Manager.cpp
@@ -38,6 +38,11 @@ void Manager::configure(osgViewer::View &view) const
     }
 }
 
+const char *Manager::getSystemName() const
+{
+    return _state->getSystemName();
+}
+
 void Manager::addMirror(Mirror *mirror)
 {
     if (!_state.valid() || !_state->valid())

include/osgXR/Manager
@@ -35,6 +35,13 @@ class Manager : public osgViewer::ViewConfig
         virtual void configure(osgViewer::View& view) const;
 
         /*
+         * OpenXR information.
+         */
+
+        /// Find the name of the OpenXR system in use.
+        const char *getSystemName() const;
+
+        /*
          * For implementation by derived classes.
          */
 

src/OpenXR/System.cpp
@@ -3,6 +3,8 @@
 
 #include "System.h"
 
+#include <cstring>
+
 using namespace osgXR;
 using namespace OpenXR;
 
@@ -15,6 +17,7 @@ void System::getProperties() const
     if (check(xrGetSystemProperties(getXrInstance(), _systemId, &properties),
               "Failed to get OpenXR system properties"))
     {
+        memcpy(_systemName, properties.systemName, sizeof(_systemName));
         _orientationTracking = properties.trackingProperties.orientationTracking;
         _positionTracking = properties.trackingProperties.positionTracking;
     }

src/OpenXR/System.h
@@ -62,6 +62,13 @@ class System
 
         void getProperties() const;
 
+        inline const char *getSystemName() const
+        {
+            if (!_readProperties)
+                getProperties();
+            return _systemName;
+        }
+
         inline bool getOrientationTracking() const
         {
             if (!_readProperties)
@@ -195,7 +202,8 @@ class System
         Instance *_instance;
         XrSystemId _systemId;
 
-        // Tracking Properties
+        // Properties
+        mutable char _systemName[XR_MAX_SYSTEM_NAME_SIZE];
         mutable bool _readProperties;
         mutable bool _orientationTracking;
         mutable bool _positionTracking;

src/XRState.h
@@ -174,6 +174,13 @@ class XRState : public osg::Referenced
             return _passesPerView;
         }
 
+        inline const char *getSystemName() const
+        {
+            if (!_valid)
+                return nullptr;
+            return _system->getSystemName();
+        }
+
         inline bool valid() const
         {
             return _valid;

commit 5d65215ba709e04d930773984c2db2aa4aa9e613

Manager: Fix getSystemName() pre init
If Manager::getSystemName() is called before initialisation, it
dereferences a null _state pointer. In that case return "" instead.

Fixes: 08efbe1d80a1 ("Manager: Expose system name")
Signed-off-by: James Hogan <james@albanarts.com>

src/Manager.cpp
@@ -42,7 +42,10 @@ void Manager::configure(osgViewer::View &view) const
 
 const char *Manager::getSystemName() const
 {
-    return _state->getSystemName();
+    if (_state.valid())
+        return _state->getSystemName();
+    else
+        return "";
 }
 
 void Manager::addMirror(Mirror *mirror)