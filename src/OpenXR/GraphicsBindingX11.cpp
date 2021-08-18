1f8ab0d49a2130b8ca634336f57c917595f9aa01
James Hogan <james@albanarts.com>
2021-08-18 17:23:48 +0100

    GraphicsBindingX11: Hack to get visualid and FBConfig
    Use a bit of a hack to get protected member data from GraphicsWindowX11
    that don't have public accessors to put in the OpenXR graphics binding.
    
    A derived class extends GraphicsWindowX11 with accessors for _visualInfo
    and _fbConfig, and GraphicsBindingX11::GraphicsBindingImpl() casts the
    GraphicsWindowX11 pointer to this derived class in order to access the
    protected members.
    
    Signed-off-by: James Hogan <james@albanarts.com>

--- src/OpenXR/GraphicsBindingX11.cpp
+++ src/OpenXR/GraphicsBindingX11.cpp
@@ -6,13 +6,36 @@
 using namespace osgXR;
 using namespace OpenXR;
 
+namespace {
+
+/// Class to spy on protected members of GraphicsWindowX11.
+class GraphicsWindowX11Spy : public osgViewer::GraphicsWindowX11
+{
+public:
+    const XVisualInfo *getVisualInfo() const
+    {
+        return _visualInfo;
+    }
+
+    const GLXFBConfig &getFBConfig() const
+    {
+        return _fbConfig;
+    }
+};
+
+}
+
 template <>
 GraphicsBindingX11::GraphicsBindingImpl(osgViewer::GraphicsWindowX11 *window) :
     _binding{ XR_TYPE_GRAPHICS_BINDING_OPENGL_XLIB_KHR }
 {
+    // window isn't actually of type GraphicsWindowX11Spy, but this allows us to
+    // spy on protected members that don't have public accessors.
+    auto spyWindow = static_cast<GraphicsWindowX11Spy *>(window);
+
     _binding.xDisplay = window->getDisplay();
-    //_binding.visualid = window->_visualInfo->visualid;
-    //_binding.glxFBConfig = window->_fbConfig;
+    _binding.visualid = spyWindow->getVisualInfo()->visualid;
+    _binding.glxFBConfig = spyWindow->getFBConfig();
     _binding.glxDrawable = window->getWindow();
     _binding.glxContext = window->getContext();
 }
