amalon/osgXR

commit 1b218ed001f8b8171930c115588eb863ed8bdc8c

Handle concurrent frames
Handle concurrent frames being accessed, such as when the next frame is
started for update prior to the previous one finishing rendering.

The single mutex locked XRState::_frame is replaced by a new FrameStore
class which has a simple array to store a couple of frames concurrently,
and methods for safely retrieving, creating, or ending a frame.

Frames are distinguished by osg::FrameStamp::getFrameNumber(), with
osg::FrameStamp pointers being passed around XRState and to FrameStore.

Signed-off-by: James Hogan <james@albanarts.com>

src/FrameStore.cpp
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#include "FrameStore.h"
+
+#include <osg/FrameStamp>
+
+#include <cassert>
+
+using namespace osgXR;
+
+FrameStore::FrameStore()
+{
+}
+
+osg::ref_ptr<FrameStore::Frame> FrameStore::getFrame(FrameStore::Stamp stamp)
+{
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+
+    int index = lookupFrame(stamp);
+    if (index < 0)
+        return nullptr;
+
+    return _store[index];
+}
+
+osg::ref_ptr<FrameStore::Frame> FrameStore::getFrame(FrameStore::Stamp stamp,
+                                                     OpenXR::Session *session)
+{
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+
+    int index = lookupFrame(stamp);
+    if (index < 0)
+    {
+        index = blankFrame();
+        // there surely shouldn't be more than 2 frames in parallel
+        assert(index >= 0);
+        if (index < 0)
+            return nullptr;
+
+        osg::ref_ptr<OpenXR::Session::Frame> frame = session->waitFrame();
+        if (frame.valid())
+        {
+            frame->setOsgFrameNumber(stamp->getFrameNumber());
+            _store[index] = frame;
+        }
+        return frame;
+    }
+
+    return _store[index];
+}
+
+bool FrameStore::endFrame(FrameStore::Stamp stamp)
+{
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+
+    int index = lookupFrame(stamp);
+    if (index < 0)
+        return false;
+
+    _store[index]->end();
+    _store[index] = nullptr;
+
+    return true;
+}
+
+int FrameStore::lookupFrame(FrameStore::Stamp stamp) const
+{
+    unsigned int frameNumber = stamp->getFrameNumber();
+    for (int i = 0; i < maxFrames; ++i)
+    {
+        if (_store[i].valid() &&
+            _store[i]->getOsgFrameNumber() == frameNumber)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int FrameStore::blankFrame() const
+{
+    for (int i = 0; i < maxFrames; ++i)
+        if (!_store[i].valid())
+            return i;
+    return -1;
+}

src/CMakeLists.txt
@@ -24,6 +24,7 @@ set(osgXR_SRCS
     XRFramebuffer.cpp
     XRState.cpp
     XRRealizeOperation.cpp
+    FrameStore.cpp
     Manager.cpp
     OpenXRDisplay.cpp
     Settings.cpp

src/FrameStore.h
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_FRAME_STORE
+#define OSGXR_FRAME_STORE 1
+
+#include "OpenXR/Session.h"
+
+#include <osg/ref_ptr>
+#include <OpenThreads/Mutex>
+
+#include <vector>
+
+namespace osg {
+    class FrameStamp;
+}
+
+namespace osgXR {
+
+/**
+ * Manages concurrent frames.
+ * A FrameStore stores any concurrent OpenXR frames and allows them to be
+ * created and retrieved in a thread-safe way based on an osg::FrameStamp.
+ */
+class FrameStore
+{
+    public:
+
+        typedef OpenXR::Session::Frame Frame;
+        typedef const osg::FrameStamp *Stamp;
+
+        FrameStore();
+
+        /// Get a frame by FrameStamp.
+        osg::ref_ptr<Frame> getFrame(Stamp stamp);
+
+        /// Get or wait for a frame by FrameStamp.
+        osg::ref_ptr<Frame> getFrame(Stamp stamp, OpenXR::Session *session);
+
+        /**
+         * End a frame by FrameStamp.
+         * @return true on success, false otherwise.
+         */
+        bool endFrame(Stamp stamp);
+
+    protected:
+
+        // These return cache index or -1
+        int lookupFrame(Stamp stamp) const;
+        int blankFrame() const;
+
+        // 2 allows work to start on next frame before the prior one has ended
+        static constexpr unsigned int maxFrames = 2;
+        // Protected by _mutex
+        osg::ref_ptr<Frame> _store[maxFrames];
+
+        // For access to _store
+        OpenThreads::Mutex _mutex;
+};
+
+}
+
+#endif

src/OpenXR/Session.cpp
@@ -188,6 +188,7 @@ Session::Frame::Frame(osg::ref_ptr<Session> session, XrFrameState *frameState) :
     _time(frameState->predictedDisplayTime),
     _period(frameState->predictedDisplayPeriod),
     _shouldRender(frameState->shouldRender),
+    _osgFrameNumber(0),
     _locatedViews(false),
     _begun(false),
     _envBlendMode(XR_ENVIRONMENT_BLEND_MODE_MAX_ENUM)

src/OpenXR/Session.h
@@ -181,6 +181,15 @@ class Session : public osg::Referenced
                     return _envBlendMode;
                 }
 
+                inline void setOsgFrameNumber(unsigned int osgFrameNumber)
+                {
+                    _osgFrameNumber = osgFrameNumber;
+                }
+                inline unsigned int getOsgFrameNumber() const
+                {
+                    return _osgFrameNumber;
+                }
+
                 void addLayer(osg::ref_ptr<CompositionLayer> layer);
 
                 // Operations
@@ -195,6 +204,9 @@ class Session : public osg::Referenced
                 XrTime _time;
                 XrDuration _period;
                 bool _shouldRender;
+
+                // OpenSceneGraph frame
+                unsigned int _osgFrameNumber;
 
                 // View locations
                 bool _locatedViews;

src/XRState.cpp
@@ -276,25 +276,18 @@ void XRState::XRView::setupCamera(osg::ref_ptr<osg::Camera> camera)
     camera->setFinalDrawCallback(new PostDrawCallback(getSwapchain()));
 }
 
-void XRState::XRView::endFrame()
+void XRState::XRView::endFrame(OpenXR::Session::Frame *frame)
 {
-    if (_state->_frame != nullptr)
+    // Add view info to projection layer for compositor
+    osg::ref_ptr<OpenXR::CompositionLayerProjection> proj = _state->getProjectionLayer();
+    if (proj != nullptr)
     {
-        // Add view info to projection layer for compositor
-        osg::ref_ptr<OpenXR::CompositionLayerProjection> proj = _state->getProjectionLayer();
-        if (proj != nullptr)
-        {
-            proj->addView(_state->_frame, _viewIndex, _swapchainSubImage,
-                          _state->_useDepthInfo ? &_state->_depthInfo : nullptr);
-        }
-        else
-        {
-            OSG_WARN << "No projection layer" << std::endl;
-        }
+        proj->addView(frame, _viewIndex, _swapchainSubImage,
+                      _state->_useDepthInfo ? &_state->_depthInfo : nullptr);
     }
     else
     {
-        OSG_WARN << "No frame" << std::endl;
+        OSG_WARN << "No projection layer" << std::endl;
     }
 }
 
@@ -608,67 +601,69 @@ void XRState::setupSceneViewCamera(osg::ref_ptr<osg::Camera> camera)
 
     // Set the stereo matrices callback on each SceneView
     osgViewer::Renderer *renderer = static_cast<osgViewer::Renderer *>(camera->getRenderer());
-    osg::ref_ptr<ComputeStereoMatricesCallback> stereoCallback = new ComputeStereoMatricesCallback(this);
     for (unsigned int i = 0; i < 2; ++i)
     {
         osgUtil::SceneView *sceneView = renderer->getSceneView(i);
-        sceneView->setComputeStereoMatricesCallback(stereoCallback);
+        sceneView->setComputeStereoMatricesCallback(
+            new ComputeStereoMatricesCallback(this, sceneView));
     }
 
     camera->setDisplaySettings(_stereoDisplaySettings);
 }
 
-void XRState::startFrame()
+osg::ref_ptr<OpenXR::Session::Frame> XRState::getFrame(osg::FrameStamp *stamp)
 {
-    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+    // Fast path
+    osg::ref_ptr<OpenXR::Session::Frame> frame = _frames.getFrame(stamp);
+    if (frame.valid())
+        return frame;
 
-    if (_frame)
-        return;
-
+    // There's probably a better place to handle events
     _instance->handleEvents();
     if (!_session->isReady())
     {
         OSG_WARN << "OpenXR session not ready for frames yet" << std::endl;
-        return;
+        return nullptr;
     }
     if (!_session->hasBegun())
     {
         if (!_session->begin(*_chosenViewConfig))
-            return;
+            return nullptr;
     }
 
-    _frame = _session->waitFrame();
+    // Slow path
+    return _frames.getFrame(stamp, _session);
 }
 
-void XRState::startRendering()
+void XRState::startRendering(osg::FrameStamp *stamp)
 {
-    startFrame();
-    if (_frame != nullptr && !_frame->hasBegun()) {
-        _frame->begin();
+    osg::ref_ptr<OpenXR::Session::Frame> frame = getFrame(stamp);
+    if (frame.valid() && !frame->hasBegun()) {
+        frame->begin();
         _projectionLayer = new OpenXR::CompositionLayerProjection(_xrViews.size());
         _projectionLayer->setLayerFlags(XR_COMPOSITION_LAYER_BLEND_TEXTURE_SOURCE_ALPHA_BIT);
         _projectionLayer->setSpace(_session->getLocalSpace());
     }
 }
 
-void XRState::endFrame()
+void XRState::endFrame(osg::FrameStamp *stamp)
 {
-    if (_frame == nullptr)
+    osg::ref_ptr<OpenXR::Session::Frame> frame = _frames.getFrame(stamp);
+    if (!frame.valid())
     {
         OSG_WARN << "OpenXR frame not waited for" << std::endl;
         return;
     }
-    if (!_frame->hasBegun())
+    if (!frame->hasBegun())
     {
         OSG_WARN << "OpenXR frame not begun" << std::endl;
         return;
     }
     for (auto &view: _xrViews)
-        view->endFrame();
-    _frame->setEnvBlendMode(_chosenEnvBlendMode);
-    _frame->addLayer(_projectionLayer.get());
-    _frame->end();
-    _frame = nullptr;
+        view->endFrame(frame);
+    frame->setEnvBlendMode(_chosenEnvBlendMode);
+    frame->addLayer(_projectionLayer.get());
+    _frames.endFrame(stamp);
 }
 
 void XRState::updateSlave(uint32_t viewIndex, osg::View& view,
@@ -677,10 +672,10 @@ void XRState::updateSlave(uint32_t viewIndex, osg::View& view,
     bool setProjection = false;
     osg::Matrix projectionMatrix;
 
-    startFrame();
-    if (_frame != nullptr) {
-        if (_frame->isPositionValid() && _frame->isOrientationValid()) {
-            const auto &pose = _frame->getViewPose(viewIndex);
+    osg::ref_ptr<OpenXR::Session::Frame> frame = getFrame(view.getFrameStamp());
+    if (frame.valid()) {
+        if (frame->isPositionValid() && frame->isOrientationValid()) {
+            const auto &pose = frame->getViewPose(viewIndex);
             osg::Vec3 position(pose.position.x,
                                pose.position.y,
                                pose.position.z);
@@ -700,7 +695,7 @@ void XRState::updateSlave(uint32_t viewIndex, osg::View& view,
                                                                bottom, top,
                                                                zNear, zFar))
             {
-                const auto &fov = _frame->getViewFov(viewIndex);
+                const auto &fov = frame->getViewFov(viewIndex);
                 createProjectionFov(projectionMatrix, fov, zNear, zFar);
                 setProjection = true;
             }
@@ -715,16 +710,18 @@ void XRState::updateSlave(uint32_t viewIndex, osg::View& view,
     }
 }
 
-osg::Matrixd XRState::getEyeProjection(uint32_t viewIndex, const osg::Matrixd& projection)
+osg::Matrixd XRState::getEyeProjection(osg::FrameStamp *stamp,
+                                       uint32_t viewIndex,
+                                       const osg::Matrixd& projection)
 {
-    startFrame();
-    if (_frame != nullptr) {
+    osg::ref_ptr<OpenXR::Session::Frame> frame = getFrame(stamp);
+    if (frame.valid()) {
         double left, right, bottom, top, zNear, zFar;
         if (projection.getFrustum(left, right,
                                   bottom, top,
                                   zNear, zFar))
         {
-            const auto &fov = _frame->getViewFov(viewIndex);
+            const auto &fov = frame->getViewFov(viewIndex);
             osg::Matrix projectionMatrix;
             createProjectionFov(projectionMatrix, fov, zNear, zFar);
             return projectionMatrix;
@@ -733,12 +730,13 @@ osg::Matrixd XRState::getEyeProjection(uint32_t viewIndex, const osg::Matrixd& p
     return projection;
 }
 
-osg::Matrixd XRState::getEyeView(uint32_t viewIndex, const osg::Matrixd& view)
+osg::Matrixd XRState::getEyeView(osg::FrameStamp *stamp, uint32_t viewIndex,
+                                 const osg::Matrixd& view)
 {
-    startFrame();
-    if (_frame != nullptr) {
-        if (_frame->isPositionValid() && _frame->isOrientationValid()) {
-            const auto &pose = _frame->getViewPose(viewIndex);
+    osg::ref_ptr<OpenXR::Session::Frame> frame = getFrame(stamp);
+    if (frame.valid()) {
+        if (frame->isPositionValid() && frame->isOrientationValid()) {
+            const auto &pose = frame->getViewPose(viewIndex);
             osg::Vec3 position(pose.position.x,
                                pose.position.y,
                                pose.position.z);
@@ -767,7 +765,7 @@ void XRState::initialDrawCallback(osg::RenderInfo &renderInfo)
         renderer->setCameraRequiresSetUp(false);
     }
 
-    startRendering();
+    startRendering(renderInfo.getState()->getFrameStamp());
 
     // Get up to date depth info from camera's projection matrix
     _depthInfo.setZRangeFromProjection(renderInfo.getCurrentCamera()->getProjectionMatrix());
@@ -778,7 +776,7 @@ void XRState::swapBuffersImplementation(osg::GraphicsContext* gc)
     // Submit rendered frame to compositor
     //m_device->submitFrame();
 
-    endFrame();
+    endFrame(gc->getState()->getFrameStamp());
 
     // Blit mirror texture to backbuffer
     //m_device->blitMirrorTexture(gc);

src/XRState.h
@@ -13,6 +13,7 @@
 #include "OpenXR/DepthInfo.h"
 
 #include "XRFramebuffer.h"
+#include "FrameStore.h"
 
 #include <osg/DisplaySettings>
 #include <osg/Referenced>
@@ -23,6 +24,10 @@
 #include <osgXR/View>
 
 #include <vector>
+
+namespace osg {
+    class FrameStamp;
+}
 
 namespace osgXR {
 
@@ -99,7 +104,7 @@ class XRState : public osg::Referenced
 
                 void setupCamera(osg::ref_ptr<osg::Camera> camera);
 
-                void endFrame();
+                void endFrame(OpenXR::Session::Frame *frame);
 
             protected:
 
@@ -161,15 +166,18 @@ class XRState : public osg::Referenced
         void init(osgViewer::GraphicsWindow *window,
                   osgViewer::View *view);
 
-        void startFrame();
-        void startRendering();
-        void endFrame();
+        osg::ref_ptr<OpenXR::Session::Frame> getFrame(osg::FrameStamp *stamp);
+        void startRendering(osg::FrameStamp *stamp);
+        void endFrame(osg::FrameStamp *stamp);
 
         void updateSlave(uint32_t viewIndex, osg::View& view,
                          osg::View::Slave& slave);
 
-        osg::Matrixd getEyeProjection(uint32_t viewIndex, const osg::Matrixd& projection);
-        osg::Matrixd getEyeView(uint32_t viewIndex, const osg::Matrixd& view);
+        osg::Matrixd getEyeProjection(osg::FrameStamp *stamp,
+                                      uint32_t viewIndex,
+                                      const osg::Matrixd& projection);
+        osg::Matrixd getEyeView(osg::FrameStamp *stamp, uint32_t viewIndex,
+                                const osg::Matrixd& view);
 
         void initialDrawCallback(osg::RenderInfo &renderInfo);
         void swapBuffersImplementation(osg::GraphicsContext* gc);
@@ -211,13 +219,10 @@ class XRState : public osg::Referenced
         osg::ref_ptr<OpenXR::Session> _session;
         std::vector<osg::ref_ptr<XRView> > _xrViews;
         std::vector<osg::ref_ptr<AppView> > _appViews;
-        osg::ref_ptr<OpenXR::Session::Frame> _frame;
+        FrameStore _frames;
         osg::ref_ptr<OpenXR::CompositionLayerProjection> _projectionLayer;
         OpenXR::DepthInfo _depthInfo;
         osg::ref_ptr<osg::DisplaySettings> _stereoDisplaySettings;
-
-        // frames can be started from cull or rendering threads
-        OpenThreads::Mutex _mutex;
 };
 
 } // osgXR

src/XRStateCallbacks.h
@@ -39,34 +39,41 @@ class ComputeStereoMatricesCallback : public osgUtil::SceneView::ComputeStereoMa
 {
     public:
 
-        ComputeStereoMatricesCallback(osg::ref_ptr<XRState> xrState) :
-            _xrState(xrState)
+        ComputeStereoMatricesCallback(XRState *xrState,
+                                      osgUtil::SceneView *sceneView) :
+            _xrState(xrState),
+            _sceneView(sceneView)
         {
         }
 
         virtual osg::Matrixd computeLeftEyeProjection(const osg::Matrixd& projection) const
         {
-            return _xrState->getEyeProjection(0, projection);
+            return _xrState->getEyeProjection(_sceneView->getFrameStamp(),
+                                              0, projection);
         }
 
         virtual osg::Matrixd computeLeftEyeView(const osg::Matrixd& view) const
         {
-            return _xrState->getEyeView(0, view);
+            return _xrState->getEyeView(_sceneView->getFrameStamp(),
+                                        0, view);
         }
 
         virtual osg::Matrixd computeRightEyeProjection(const osg::Matrixd& projection) const
         {
-            return _xrState->getEyeProjection(1, projection);
+            return _xrState->getEyeProjection(_sceneView->getFrameStamp(),
+                                              1, projection);
         }
 
         virtual osg::Matrixd computeRightEyeView(const osg::Matrixd& view) const
         {
-            return _xrState->getEyeView(1, view);
+            return _xrState->getEyeView(_sceneView->getFrameStamp(),
+                                        1, view);
         }
 
     protected:
 
         osg::observer_ptr<XRState> _xrState;
+        osg::observer_ptr<osgUtil::SceneView> _sceneView;
 };
 
 class InitialDrawCallback : public osg::Camera::DrawCallback

commit 708f371f66959f4507a155df2f05fb1bc92bb850

FrameStore: Fix integer comparison warnings
Fix a couple of signed / unsigned integer comparison warnings in for
loops.

Fixes: 1b218ed001f8 ("Handle concurrent frames")
Signed-off-by: James Hogan <james@albanarts.com>

src/FrameStore.cpp
@@ -67,7 +67,7 @@ bool FrameStore::endFrame(FrameStore::Stamp stamp)
 int FrameStore::lookupFrame(FrameStore::Stamp stamp) const
 {
     unsigned int frameNumber = stamp->getFrameNumber();
-    for (int i = 0; i < maxFrames; ++i)
+    for (unsigned int i = 0; i < maxFrames; ++i)
     {
         if (_store[i].valid() &&
             _store[i]->getOsgFrameNumber() == frameNumber)
@@ -80,7 +80,7 @@ int FrameStore::lookupFrame(FrameStore::Stamp stamp) const
 
 int FrameStore::blankFrame() const
 {
-    for (int i = 0; i < maxFrames; ++i)
+    for (unsigned int i = 0; i < maxFrames; ++i)
         if (!_store[i].valid())
             return i;
     return -1;