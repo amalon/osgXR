b7250084b36e6f45e4b2581bc59386f848934f24
James Hogan <james@albanarts.com>
2021-10-14 08:22:51 +0100

    Add OpenXR::Path class to wrap XrPath
    Create a class to wrap an XrPath for a specific Instance, with a
    constructor to create from a string, and a toString() accessor to
    convert back.
    
    The class isn't based on osg::Referenced as it is expected to be used as
    a thin wrapper and doesn't require lifetime management.
    
    Signed-off-by: James Hogan <james@albanarts.com>

---
+++ src/OpenXR/Path.cpp
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#include "Path.h"
+
+using namespace osgXR::OpenXR;
+
+Path::Path(Instance *instance,
+           XrPath path) :
+    _instance(instance),
+    _path(path)
+{
+}
+
+Path::Path(Instance *instance,
+           const std::string &path) :
+    _instance(instance),
+    _path(XR_NULL_PATH)
+{
+    check(xrStringToPath(getXrInstance(), path.c_str(), &_path),
+          "Failed to create OpenXR path from string");
+}
+
+std::string Path::toString() const
+{
+    if (!valid())
+        return "";
+
+    uint32_t count;
+    if (!check(xrPathToString(getXrInstance(), _path,
+                              0, &count, nullptr),
+               "Failed to size OpenXR path string"))
+        return "";
+    std::vector<char> buffer(count);
+    if (!check(xrPathToString(getXrInstance(), _path,
+                              buffer.size(), &count, buffer.data()),
+               "Failed to get OpenXR path string"))
+        return "";
+
+    return buffer.data();
+}
---
+++ src/OpenXR/Path.h
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_OPENXR_PATH
+#define OSGXR_OPENXR_PATH 1
+
+#include "Instance.h"
+
+#include <osg/ref_ptr>
+
+#include <string>
+
+namespace osgXR {
+
+namespace OpenXR {
+
+class Path
+{
+    public:
+
+        Path(Instance *instance = nullptr, XrPath path = XR_NULL_PATH);
+        Path(Instance *instance, const std::string &path);
+
+        // Error checking
+
+        inline bool valid() const
+        {
+            return _path != XR_NULL_PATH;
+        }
+
+        inline bool check(XrResult result, const char *warnMsg) const
+        {
+            return _instance->check(result, warnMsg);
+        }
+
+        // Conversions
+
+        inline const osg::ref_ptr<Instance> getInstance() const
+        {
+            return _instance;
+        }
+
+        inline XrInstance getXrInstance() const
+        {
+            return _instance->getXrInstance();
+        }
+
+        inline XrPath getXrPath() const
+        {
+            return _path;
+        }
+
+        std::string toString() const;
+
+        // Comparisons
+
+        bool operator == (const Path &other) const
+        {
+            return _path == other._path &&
+                   _instance == other._instance;
+        }
+
+        bool operator != (const Path &other) const
+        {
+            return _path != other._path ||
+                   _instance != other._instance;
+        }
+
+    protected:
+
+        // Path data
+        osg::ref_ptr<Instance> _instance;
+        XrPath _path;
+};
+
+} // osgXR::OpenXR
+
+} // osgXR
+
+#endif
