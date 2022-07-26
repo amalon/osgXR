amalon/osgXR

commit 75f202a5b2a46d3998ecdddcc54d3c2606b35b4f

Add Swapchain & SubImage APIs
Add a public API to allow the application to define swapchains for use
in upcoming composition layers.

These aren't a lot of use just yet, but they can be attached to cameras
to allow them to be rendered into, and attached to mirrors to allow the
mirror texture to be automatically updated after they've been rendered
into.

Also included is a SubImage API, for describing a subimage of a
swapchain. This is defined purely in the header and is intended to be
passed by reference rather than reference counted. It mostly just wraps
a Swapchain smart pointer with a few extra parameters, including a
currently unused array index which may be coded up in future.

Signed-off-by: James Hogan <james@albanarts.com>

src/Swapchain.cpp
@@ -1,0 +1,424 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#include "Swapchain.h"
+
+#include "OpenXR/System.h"
+
+#include "XRState.h"
+
+#include <osg/Notify>
+#include <osg/observer_ptr>
+
+#include <osgViewer/Renderer>
+
+#include <memory>
+#include <sstream>
+
+using namespace osgXR;
+
+namespace {
+
+// Draw callbacks
+
+class InitialDrawCallback : public osg::Camera::DrawCallback
+{
+    public:
+        InitialDrawCallback(std::shared_ptr<Swapchain::Private> &swapchain) :
+            _swapchain(swapchain)
+        {
+        }
+
+        void operator()(osg::RenderInfo& renderInfo) const override
+        {
+            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            if (swapchain)
+                swapchain->initialDrawCallback(renderInfo);
+        }
+
+    protected:
+        std::weak_ptr<Swapchain::Private> _swapchain;
+};
+
+class PreDrawCallback : public osg::Camera::DrawCallback
+{
+    public:
+        PreDrawCallback(std::shared_ptr<Swapchain::Private> &swapchain) :
+            _swapchain(swapchain)
+        {
+            swapchain->incNumDrawPasses();
+        }
+
+        ~PreDrawCallback()
+        {
+            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            if (swapchain)
+                swapchain->decNumDrawPasses();
+        }
+
+        void operator()(osg::RenderInfo& renderInfo) const override
+        {
+            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            if (swapchain)
+                swapchain->preDrawCallback(renderInfo);
+        }
+
+    protected:
+        std::weak_ptr<Swapchain::Private> _swapchain;
+};
+
+class PostDrawCallback : public osg::Camera::DrawCallback
+{
+    public:
+        PostDrawCallback(std::shared_ptr<Swapchain::Private> &swapchain) :
+            _swapchain(swapchain)
+        {
+        }
+
+        void operator()(osg::RenderInfo& renderInfo) const override
+        {
+            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            if (swapchain)
+                swapchain->postDrawCallback(renderInfo);
+        }
+
+    protected:
+        std::weak_ptr<Swapchain::Private> _swapchain;
+};
+
+}
+
+// Internal API
+
+Swapchain::Private::Private(uint32_t width, uint32_t height) :
+    _preferredRGBEncodingMask(0),
+    _allowedRGBEncodingMask(0),
+    _rgbBits(10),  // 8-bits is unlikely to be sufficient for linear encodings
+    _alphaBits(0), // Alpha channel not required by default
+    _width(width),
+    _height(height),
+    _forcedAlpha(-1.0f),
+    _numDrawPasses(0),
+    _updated(true)
+{
+}
+
+Swapchain::Private::~Private()
+{
+}
+
+void Swapchain::Private::attachToCamera(std::shared_ptr<Private> &self,
+                                        osg::Camera *camera)
+{
+    camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER);
+    camera->setInitialDrawCallback(new InitialDrawCallback(self));
+    camera->setPreDrawCallback(new PreDrawCallback(self));
+    camera->setFinalDrawCallback(new PostDrawCallback(self));
+
+    // FIXME Do all cameras inherit the main display settings... eeek!
+    //camera->setDisplaySettings(osg::DisplaySettings::instance());
+
+    if (_swapchain.valid())
+        _swapchain->incNumDrawPasses(1); // FIXME HACK depends on where node is!
+}
+
+void Swapchain::Private::attachToMirror(std::shared_ptr<Private> &self,
+                                        osg::StateSet *stateSet)
+{
+    _stateSets.push_back(stateSet);
+}
+
+void Swapchain::Private::preferRGBEncoding(Encoding encoding)
+{
+    uint32_t mask = (1u << (unsigned int)encoding);
+    _preferredRGBEncodingMask |= mask;
+    _allowedRGBEncodingMask |= mask;
+}
+
+void Swapchain::Private::allowRGBEncoding(Encoding encoding)
+{
+    uint32_t mask = (1u << (unsigned int)encoding);
+    _allowedRGBEncodingMask |= mask;
+}
+
+void Swapchain::Private::setRGBBits(unsigned int rgbBits)
+{
+    _rgbBits = rgbBits;
+}
+
+unsigned int Swapchain::Private::getRGBBits() const
+{
+    return _rgbBits;
+}
+
+void Swapchain::Private::setAlphaBits(unsigned int alphaBits)
+{
+    _alphaBits = alphaBits;
+}
+
+unsigned int Swapchain::Private::getAlphaBits() const
+{
+    return _alphaBits;
+}
+
+void Swapchain::Private::setWidth(uint32_t width)
+{
+    if (width != _width)
+        _updated = true;
+    _width = width;
+}
+
+uint32_t Swapchain::Private::getWidth() const
+{
+    return _width;
+}
+
+void Swapchain::Private::setHeight(uint32_t height)
+{
+    if (height != _height)
+        _updated = true;
+    _height = height;
+}
+
+uint32_t Swapchain::Private::getHeight() const
+{
+    return _height;
+}
+
+void Swapchain::Private::setForcedAlpha(float alpha)
+{
+    if (alpha < 0.0f)
+        alpha = 0.0f;
+    if (alpha > 1.0f)
+        alpha = 1.0f;
+    _forcedAlpha = alpha;
+}
+
+void Swapchain::Private::disableForcedAlpha()
+{
+    _forcedAlpha = -1.0f;
+}
+
+float Swapchain::Private::getForcedAlpha() const
+{
+    return _forcedAlpha;
+}
+
+bool Swapchain::Private::setup(XRState *state, OpenXR::Session *session)
+{
+    XRState *oldState = _state.get();
+    if (oldState)
+    {
+        if (oldState != state)
+        {
+            OSG_WARN << "Swapchain XRState conflict" << std::endl;
+            return false;
+        }
+        if (!_updated)
+            return true;
+    }
+
+    OpenXR::System::ViewConfiguration::View view(_width, _height);
+    int64_t rgbaFormat = state->chooseRGBAFormat(_rgbBits, _alphaBits,
+                                                 _preferredRGBEncodingMask,
+                                                 _allowedRGBEncodingMask);
+    if (!rgbaFormat)
+    {
+        std::stringstream formats;
+        formats << std::hex;
+        for (int64_t format: session->getSwapchainFormats())
+            formats << " 0x" << format;
+        OSG_WARN << "Swapchain setup: No supported swapchain format found in ["
+                 << formats.str() << " ]" << std::endl;
+        return false;
+    }
+
+    _state = state;
+    _updated = false;
+    _session = session;
+    _swapchain = new XRState::XRSwapchain(state, session,
+                                          view, rgbaFormat,
+                                          0, GL_DEPTH_COMPONENT16);
+    _swapchain->setForcedAlpha(_forcedAlpha);
+    _swapchain->incNumDrawPasses(_numDrawPasses);
+
+    return true;
+}
+
+bool Swapchain::Private::sync()
+{
+    if (_updated && _session.valid())
+        return setup(_state.get(), _session.get());
+    return true;
+}
+
+void Swapchain::Private::cleanupSession()
+{
+    _swapchain = nullptr;
+    _session = nullptr;
+    _state = nullptr;
+}
+
+bool Swapchain::Private::valid() const
+{
+    return _swapchain.valid();
+}
+
+void Swapchain::Private::initialDrawCallback(osg::RenderInfo &renderInfo)
+{
+    if (_swapchain.valid())
+    {
+        // FIXME this isn't the ideal place, but it'll probably do in practice
+        // due to lack of concurrency
+        sync();
+        osg::GraphicsOperation *graphicsOperation = renderInfo.getCurrentCamera()->getRenderer();
+        osgViewer::Renderer *renderer = dynamic_cast<osgViewer::Renderer*>(graphicsOperation);
+        if (renderer != nullptr)
+        {
+            // Disable normal OSG FBO camera setup because it will undo the MSAA FBO configuration.
+            renderer->setCameraRequiresSetUp(false);
+        }
+
+        _state->startRendering(renderInfo.getState()->getFrameStamp());
+    }
+}
+
+void Swapchain::Private::preDrawCallback(osg::RenderInfo &renderInfo)
+{
+    if (_swapchain.valid())
+        _swapchain->preDrawCallback(renderInfo);
+}
+
+void Swapchain::Private::postDrawCallback(osg::RenderInfo &renderInfo)
+{
+    if (_swapchain.valid())
+    {
+        _swapchain->setForcedAlpha(_forcedAlpha);
+        _swapchain->postDrawCallback(renderInfo);
+
+        const osg::FrameStamp *stamp = renderInfo.getState()->getFrameStamp();
+        auto texture = _swapchain->getOsgTexture(stamp);
+        if (texture.valid())
+        {
+            for (auto it = _stateSets.begin(); it != _stateSets.end(); ++it)
+            {
+                osg::ref_ptr<osg::StateSet> stateSet = *it;
+                if (stateSet.valid())
+                    // update state set's texture object
+                    stateSet->setTextureAttributeAndModes(0, texture);
+                else
+                    // clean up after stale state sets
+                    it = _stateSets.erase(it);
+            }
+        }
+
+        // FIXME somewhere we should remove or reset the texture:
+        // stateSet->removeTextureAttribute(0, osg::StateAttribute::Type::TEXTURE);
+    }
+}
+
+OpenXR::SwapchainGroupSubImage Swapchain::Private::convertSubImage(const SubImage &subImage) const
+{
+    OpenXR::System::ViewConfiguration::View::Viewport vp = {};
+    vp.x = subImage.getX();
+    vp.y = subImage.getY();
+    vp.width = subImage.getWidth();
+    if (!vp.width)
+        vp.width = _width;
+    vp.height = subImage.getHeight();
+    if (!vp.height)
+        vp.height = _height;
+    return OpenXR::SwapchainGroupSubImage(_swapchain.get(), vp);
+}
+
+// Public API
+
+Swapchain::Swapchain(uint32_t width, uint32_t height) :
+    _private(new Private(width, height))
+{
+}
+
+Swapchain::~Swapchain()
+{
+}
+
+void Swapchain::attachToCamera(osg::Camera *camera)
+{
+    _private->attachToCamera(_private, camera);
+}
+
+void Swapchain::attachToMirror(osg::StateSet *stateSet)
+{
+    _private->attachToMirror(_private, stateSet);
+}
+
+void Swapchain::preferRGBEncoding(Swapchain::Encoding encoding)
+{
+    _private->preferRGBEncoding(encoding);
+}
+
+void Swapchain::allowRGBEncoding(Swapchain::Encoding encoding)
+{
+    _private->allowRGBEncoding(encoding);
+}
+
+void Swapchain::setRGBBits(unsigned int rgbBits)
+{
+    _private->setRGBBits(rgbBits);
+}
+
+unsigned int Swapchain::getRGBBits() const
+{
+    return _private->getRGBBits();
+}
+
+void Swapchain::setAlphaBits(unsigned int alphaBits)
+{
+    _private->setAlphaBits(alphaBits);
+}
+
+unsigned int Swapchain::getAlphaBits() const
+{
+    return _private->getAlphaBits();
+}
+
+void Swapchain::setSize(uint32_t width, uint32_t height)
+{
+    _private->setWidth(width);
+    _private->setHeight(height);
+}
+
+void Swapchain::setWidth(uint32_t width)
+{
+    _private->setWidth(width);
+}
+
+uint32_t Swapchain::getWidth() const
+{
+    return _private->getWidth();
+}
+
+void Swapchain::setHeight(uint32_t height)
+{
+    _private->setHeight(height);
+}
+
+uint32_t Swapchain::getHeight() const
+{
+    return _private->getHeight();
+}
+
+void Swapchain::setForcedAlpha(float alpha)
+{
+    _private->setForcedAlpha(alpha);
+}
+
+void Swapchain::disableForcedAlpha()
+{
+    _private->disableForcedAlpha();
+}
+
+float Swapchain::getForcedAlpha() const
+{
+    return _private->getForcedAlpha();
+}

include/osgXR/SubImage
@@ -1,0 +1,116 @@
+// -*-c++-*-
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_SubImage
+#define OSGXR_SubImage 1
+
+#include <osgXR/Swapchain>
+
+#include <osg/ref_ptr>
+
+namespace osgXR {
+
+/**
+ * Represents an OpenXR swapchain subimage.
+ */
+class SubImage
+{
+    public:
+
+        /**
+         * Construct a subimage representing entire swapchain image.
+         * @param swapchain The swapchain (optional).
+         */
+        SubImage(Swapchain *swapchain = nullptr) :
+            _swapchain(swapchain),
+            _x(0),
+            _y(0),
+            _width(0),
+            _height(0),
+            _arrayIndex(0)
+        {
+        }
+
+        /**
+         * Construct a subimage representing part of a swapchain image.
+         * @param swapchain The swapchain.
+         * @param x         X offset in pixels.
+         * @param y         Y offset in pixels.
+         * @param width     Width in pixels.
+         * @param height    Height in pixels.
+         */
+        SubImage(Swapchain *swapchain,
+                 int32_t x, int32_t y,
+                 uint32_t width, uint32_t height) :
+            _swapchain(swapchain),
+            _x(x),
+            _y(y),
+            _width(width),
+            _height(height),
+            _arrayIndex(0)
+        {
+        }
+
+        // Accessors
+
+        /// Get swapchain pointer.
+        Swapchain *getSwapchain() const
+        {
+            return _swapchain;
+        }
+
+        /// Set offset coordinates in pixels.
+        void setOffset(int32_t x, int32_t y)
+        {
+            _x = x;
+            _y = y;
+        }
+        /// Get X offset in pixels.
+        int32_t getX() const
+        {
+            return _x;
+        }
+        /// Get Y offset in pixels.
+        int32_t getY() const
+        {
+            return _y;
+        }
+
+        /// Set extent in pixels (0 means the whole image).
+        void setExtent(uint32_t width, uint32_t height)
+        {
+            _width = width;
+            _height = height;
+        }
+        /// Get width in pixels (0 means the whole image).
+        uint32_t getWidth() const
+        {
+            return _width;
+        }
+        /// Get height in pixels (0 means the whole image).
+        uint32_t getHeight() const
+        {
+            return _height;
+        }
+
+    protected:
+
+        /// The swapchain this subimage refers to.
+        osg::ref_ptr<Swapchain> _swapchain;
+
+        /// Offset into swapchain images in pixels.
+        int32_t _x;
+        int32_t _y;
+
+        /// Size in pixels (0 means the whole image).
+        uint32_t _width;
+        uint32_t _height;
+
+        /// Image array index (must be 0 for now).
+        uint32_t _arrayIndex;
+};
+
+}
+
+#endif

include/osgXR/Swapchain
@@ -1,0 +1,110 @@
+// -*-c++-*-
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_Swapchain
+#define OSGXR_Swapchain 1
+
+#include <osgXR/Export>
+#include <osgXR/Settings>
+
+#include <osg/Camera>
+#include <osg/Referenced>
+#include <osg/StateSet>
+
+#include <memory>
+
+namespace osgXR {
+
+/**
+ * Represents an OpenXR swapchain.
+ */
+class OSGXR_EXPORT Swapchain : public osg::Referenced
+{
+    public:
+
+        /// Constructor.
+        Swapchain(uint32_t width, uint32_t height);
+
+        /// Destructor.
+        ~Swapchain();
+
+        /// Attach the swapchain to a camera.
+        void attachToCamera(osg::Camera *camera);
+        /// Update a mirror texture each time the swapchain is rewritten.
+        void attachToMirror(osg::StateSet *stateSet);
+
+        // Accessors
+
+        typedef Settings::Encoding Encoding;
+        /**
+         * Specify a preferred RGB encoding.
+         * The chosen RGB encoding is allowed for use, and a format with this
+         * encoding will be chosen in preference to other allowed encodings
+         * specified by allowRGBEncoding() if possible.
+         * @param encoding RGB encoding to prefer.
+         */
+        void preferRGBEncoding(Encoding encoding);
+        /**
+         * Specify an allowed RGB encoding.
+         * The chosen RGB encoding is allowed for use, and a format with this
+         * encoding may be chosen when none of the preferred color encodings
+         * specified by preferRGBEncoding() are useable.
+         * @param encoding RGB encoding to permit.
+         */
+        void allowRGBEncoding(Encoding encoding);
+
+        /**
+         * Set number of desired bits for each RGB channel.
+         * This only applies to linear RGB formats, not sRGB formats.
+         */
+        void setRGBBits(unsigned int rgbBits);
+        /**
+         * Get number of desired bits for each RGB channel.
+         * This only applies to linear RGB formats, not sRGB formats.
+         */
+        unsigned int getRGBBits() const;
+
+        /// Set the number of desired alpha bits.
+        void setAlphaBits(unsigned int alphaBits);
+        /// Get number of desired alpha bits.
+        unsigned int getAlphaBits() const;
+
+        /// Set required size of swapchain images in pixels.
+        void setSize(uint32_t width, uint32_t height);
+        /// Set required width of swapchain images in pixels.
+        void setWidth(uint32_t width);
+        /// Get required width of swapchain images in pixels.
+        uint32_t getWidth() const;
+
+        /// Set required height of swapchain images in pixels.
+        void setHeight(uint32_t height);
+        /// Get required height of swapchain images in pixels.
+        uint32_t getHeight() const;
+
+        /**
+         * Force a particular alpha value before releasing images.
+         * This clears the alpha channel with a particular alpha value prior to
+         * releasing the images. This can be used in conjuction with an
+         * unpremultiplied alpha composition layer to implement alpha blending.
+         * @param alpha Alpha value to force [0..1].
+         */
+        void setForcedAlpha(float alpha);
+        /// Disable forced alpha (default).
+        void disableForcedAlpha();
+        /// Get the forced alpha value or negative if disabled.
+        float getForcedAlpha() const;
+
+        class Private;
+
+    private:
+
+        std::shared_ptr<Private> _private;
+
+        // Copying not permitted
+        Swapchain(const Swapchain &copy);
+};
+
+}
+
+#endif

src/CMakeLists.txt
@@ -20,7 +20,9 @@ set(osgXR_HEADERS
     include/osgXR/MirrorSettings
     include/osgXR/OpenXRDisplay
     include/osgXR/Settings
+    include/osgXR/SubImage
     include/osgXR/Subaction
+    include/osgXR/Swapchain
     include/osgXR/View
     include/osgXR/osgXR
 )
@@ -54,6 +56,7 @@ set(osgXR_SRCS
     OpenXRDisplay.cpp
     Settings.cpp
     Subaction.cpp
+    Swapchain.cpp
     View.cpp
     osgXR.cpp
     projection.cpp

src/Swapchain.h
@@ -1,0 +1,115 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_SWAPCHAIN
+#define OSGXR_SWAPCHAIN 1
+
+#include <osgXR/SubImage>
+#include <osgXR/Swapchain>
+
+#include "OpenXR/Session.h"
+#include "OpenXR/SwapchainGroupSubImage.h"
+
+#include "XRState.h"
+
+#include <osg/Camera>
+#include <osg/observer_ptr>
+
+namespace osgXR {
+
+class XRState;
+
+class Swapchain::Private
+{
+    public:
+
+        static Private *get(Swapchain *pub)
+        {
+            return pub->_private.get();
+        }
+
+        Private(uint32_t width, uint32_t height);
+        virtual ~Private();
+
+        void attachToCamera(std::shared_ptr<Private> &self,
+                            osg::Camera *camera);
+        void attachToMirror(std::shared_ptr<Private> &self,
+                            osg::StateSet *stateSet);
+
+        // Accessors
+
+        void preferRGBEncoding(Encoding encoding);
+        void allowRGBEncoding(Encoding encoding);
+        void setRGBBits(unsigned int rgbBits);
+        unsigned int getRGBBits() const;
+        void setAlphaBits(unsigned int alphaBits);
+        unsigned int getAlphaBits() const;
+
+        void setWidth(uint32_t width);
+        uint32_t getWidth() const;
+        void setHeight(uint32_t height);
+        uint32_t getHeight() const;
+
+        void setForcedAlpha(float alpha);
+        void disableForcedAlpha();
+        float getForcedAlpha() const;
+
+        // Internal API
+
+        /// Setup swapchain with an OpenXR session
+        bool setup(XRState *state, OpenXR::Session *session);
+
+        /// Synchronise any app changes, such as resizes
+        bool sync();
+
+        /// Clean up swapchain before an OpenXR session is destroyed
+        void cleanupSession();
+
+        /// Find whether the swapchain is valid for use.
+        bool valid() const;
+
+        void initialDrawCallback(osg::RenderInfo &renderInfo);
+        void preDrawCallback(osg::RenderInfo &renderInfo);
+        void postDrawCallback(osg::RenderInfo &renderInfo);
+
+        void incNumDrawPasses()
+        {
+            ++_numDrawPasses;
+        }
+        void decNumDrawPasses()
+        {
+            --_numDrawPasses;
+        }
+
+        OpenXR::SwapchainGroupSubImage convertSubImage(const SubImage &subImage) const;
+
+    protected:
+
+        // Format requirements
+        uint32_t _preferredRGBEncodingMask;
+        uint32_t _allowedRGBEncodingMask;
+        int _rgbBits;  // for linear RGB formats, per channel
+        int _alphaBits;
+
+        // Dimention requirements
+        uint32_t _width;
+        uint32_t _height;
+
+        // Forced alpha
+        float _forcedAlpha;
+
+        unsigned int _numDrawPasses;
+        bool _updated;
+
+        // State sets to update
+        std::list<osg::observer_ptr<osg::StateSet>> _stateSets;
+
+        // State
+        osg::observer_ptr<XRState> _state;
+        osg::observer_ptr<OpenXR::Session> _session;
+        osg::ref_ptr<XRState::XRSwapchain> _swapchain;
+};
+
+} // osgXR
+
+#endif

commit 9631ed7b567d04312c37f985b2d6b717b5c6e72c

Swapchain: Use weak_ptr::lock() to avoid exception
Use std::weak_ptr<>::lock() in the Swapchain camera callbacks rather
than casting to a std::shared_ptr<>, so as to avoid an exception if the
swapchain is destroyed before the camera. Its also cleaner and more
readable.

Fixes: 75f202a5b2a4 ("Add Swapchain & SubImage APIs")
Signed-off-by: James Hogan <james@albanarts.com>

src/Swapchain.cpp
@@ -31,7 +31,7 @@ class InitialDrawCallback : public osg::Camera::DrawCallback
 
         void operator()(osg::RenderInfo& renderInfo) const override
         {
-            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            auto swapchain = _swapchain.lock();
             if (swapchain)
                 swapchain->initialDrawCallback(renderInfo);
         }
@@ -51,14 +51,14 @@ class PreDrawCallback : public osg::Camera::DrawCallback
 
         ~PreDrawCallback()
         {
-            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            auto swapchain = _swapchain.lock();
             if (swapchain)
                 swapchain->decNumDrawPasses();
         }
 
         void operator()(osg::RenderInfo& renderInfo) const override
         {
-            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            auto swapchain = _swapchain.lock();
             if (swapchain)
                 swapchain->preDrawCallback(renderInfo);
         }
@@ -77,7 +77,7 @@ class PostDrawCallback : public osg::Camera::DrawCallback
 
         void operator()(osg::RenderInfo& renderInfo) const override
         {
-            auto swapchain = (std::shared_ptr<Swapchain::Private>)(_swapchain);
+            auto swapchain = _swapchain.lock();
             if (swapchain)
                 swapchain->postDrawCallback(renderInfo);
         }