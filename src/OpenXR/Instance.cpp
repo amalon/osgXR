amalon/osgXR

commit 9ca22276a12588871b6c0c62d95a6a9581235938

Handle Monado instance loss
When the Monado runtime is shutdown, it is reported as lost by an
XR_ERROR_INSTANCE_LOST error code from an XR call. A later call to
xrWaitFrame() will hang, so bail out of OpenXR::Session::waitFrame()
early if the instance has been lost.

We currently avoid destroying the instance due to a SteamVR bug, so
change that workaround into a conditional quirk that is only enabled for
SteamVR, allowing instance destruction with Monado.

Further calls to xrCreateInstance() don't return the
XR_ERROR_INSTANCE_LOST error code (nor does the spec list it as a
failure), or even XR_ERROR_RUNTIME_UNAVAILABLE as hinted at by the spec.
Instead Monado returns XR_ERROR_RUNTIME_FAILURE. This results in an
INIT_FAIL from Instance::init() and it gives up on re-enabling VR.

Therefore handle XR_ERROR_RUNTIME_FAILURE (and
XR_ERROR_RUNTIME_UNAVAILABLE) as INIT_LATER so that the update() loop
polls again later, and increase the poll period from 100 updates up to
500 to avoid polling too frequently.

Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXR/Instance.cpp
@@ -260,9 +260,15 @@ Instance::InitResult Instance::init(const char *appName, uint32_t appVersion)
     if (XR_FAILED(res))
     {
         OSG_WARN << "Failed to create OpenXR instance: " << res << std::endl;
-        if (res == XR_ERROR_INSTANCE_LOST)
+        switch (res)
+        {
+        case XR_ERROR_RUNTIME_UNAVAILABLE:
+        case XR_ERROR_RUNTIME_FAILURE: // Monado returns this when not running
             return INIT_LATER;
-        return INIT_FAIL;
+
+        default:
+            return INIT_FAIL;
+        }
     }
 
     // Log the runtime properties

src/OpenXR/Quirks.cpp
@@ -58,6 +58,12 @@ void Quirks::probe(Instance *instance)
               MATCH_STEAMVR, MIN_XR_VERSION, MAX_XR_VERSION,
               " (https://github.com/ValveSoftware/SteamVR-for-Linux/issues/421)"),
 
+        // Since SteamVR 1.15.x apps hang during xrDestroyInstance.
+        QUIRK(QUIRK_AVOID_DESTROY_INSTANCE,
+              USING_X11,
+              MATCH_STEAMVR, MIN_XR_VERSION, MAX_XR_VERSION,
+              " (https://github.com/ValveSoftware/SteamVR-for-Linux/issues/422)"),
+
 #undef QUIRK
     };
 

src/OpenXR/Quirks.h
@@ -42,6 +42,12 @@ typedef enum Quirk {
      */
     QUIRK_GL_CONTEXT_CLEARED,
 
+    /**
+     * This quirk indicates that the app should avoid destroying the XR instance
+     * to avoid hangs.
+     */
+    QUIRK_AVOID_DESTROY_INSTANCE,
+
     QUIRK_MAX
 } Quirk;
 

src/OpenXR/Session.cpp
@@ -428,6 +428,9 @@ void Session::requestExit()
 
 osg::ref_ptr<Session::Frame> Session::waitFrame()
 {
+    if (_instance->lost())
+        return nullptr;
+
     osg::ref_ptr<Frame> frame;
 
     XrFrameWaitInfo frameWaitInfo{ XR_TYPE_FRAME_WAIT_INFO };

src/XRState.cpp
@@ -659,7 +659,7 @@ void XRState::update()
                 if (res == UP_LATER)
                 {
                     // Don't poll incessantly
-                    _upDelay = 100;
+                    _upDelay = 500;
                 }
                 break;
             }
@@ -683,7 +683,7 @@ void XRState::onInstanceLossPending(OpenXR::Instance *instance,
     // Reinitialize instance
     setDownState(VRSTATE_DISABLED);
     // FIXME use event.lossTime?
-    _upDelay = 100;
+    _upDelay = 500;
 }
 
 void XRState::onInteractionProfileChanged(OpenXR::Session *session,

src/XRState.h
@@ -320,7 +320,9 @@ class XRState : public OpenXR::EventHandler
 
         VRState getProbingState() const
         {
-            return _probing ? VRSTATE_SYSTEM : VRSTATE_DISABLED;
+            if (_instance.valid() && _instance->getQuirk(OpenXR::QUIRK_AVOID_DESTROY_INSTANCE))
+                return _probing ? VRSTATE_SYSTEM : VRSTATE_DISABLED;
+            return VRSTATE_DISABLED;
         }
 
         void setViewer(osgViewer::ViewerBase *viewer)

commit 7b7c0873e0c0448c681f00c14962d42bb4090442

Instance: Fix build with OpenXR < 1.0.16
The XR_ERROR_RUNTIME_UNAVAILABLE error code from xrCreateInstance()
superseded XR_ERROR_INSTANCE_LOST in OpenXR 1.0.16, and didn't exist
before then. Its use therefore results in compile errors with versions
of the OpenXR SDK prior to 1.0.16.

This is worked around while maintaining compatibility with a
preprocessor definition conditional upon XR_CURRENT_API_VERSION, and
XR_ERROR_INSTANCE_LOST is also handled again.

Link: https://github.com/amalon/osgXR/issues/5
Fixes: 9ca22276a125 ("Handle Monado instance loss")
Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXR/Instance.cpp
@@ -20,6 +20,11 @@
                          OSGXR_PATCH_VERSION)
 #define API_VERSION     XR_MAKE_VERSION(1, 0, 0)
 
+// Preserve compatibility with older versions of OpenXR SDK as best we can
+#if XR_CURRENT_API_VERSION < XR_MAKE_VERSION(1, 0, 16)
+#define XR_ERROR_RUNTIME_UNAVAILABLE (-51)
+#endif
+
 using namespace osgXR::OpenXR;
 
 static std::vector<XrApiLayerProperties> layers;
@@ -260,9 +265,11 @@ Instance::InitResult Instance::init(const char *appName, uint32_t appVersion)
     if (XR_FAILED(res))
     {
         OSG_WARN << "Failed to create OpenXR instance: " << res << std::endl;
-        switch (res)
+        // cast to handle XR_ERROR_RUNTIME_UNAVAILABLE as a preprocessor define
+        switch ((int)res)
         {
-        case XR_ERROR_RUNTIME_UNAVAILABLE:
+        case XR_ERROR_INSTANCE_LOST: // prior to 1.0.16
+        case XR_ERROR_RUNTIME_UNAVAILABLE: // since 1.0.16
         case XR_ERROR_RUNTIME_FAILURE: // Monado returns this when not running
             return INIT_LATER;
 