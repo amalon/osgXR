amalon/osgXR

commit 75840ac1727ef3d5197e12e73f3efe72c9ac8de7

OpenXRDisplay: Add an update operation
Set the viewer and register an update operation from OpenXRDisplay, so
that the osgteapot example can work again. It has been broken since the
introduction of the update() function.

XRState's update() code needs fixing in a few places to allow this to
work.

Fixes: 923376898803 ("Make OpenXR bringup reversible and restartable")
Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXRDisplay.cpp
@@ -8,6 +8,7 @@
 
 #include "XRState.h"
 #include "XRRealizeOperation.h"
+#include "XRUpdateOperation.h"
 
 using namespace osgXR;
 
@@ -38,5 +39,7 @@ void OpenXRDisplay::configure(osgViewer::View &view) const
         return;
 
     _state = new XRState(_settings);
+    _state->setViewer(viewer);
     viewer->setRealizeOperation(new XRRealizeOperation(_state, &view));
+    viewer->addUpdateOperation(new XRUpdateOperation(_state));
 }

src/XRRealizeOperation.cpp
@@ -29,6 +29,7 @@ void XRRealizeOperation::operator () (osg::GraphicsContext *gc)
         if (window)
         {
             _state->init(window, _view);
+            _state->setDestState(XRState::VRSTATE_ACTIONS);
             _realized = true;
         }
     }

src/CMakeLists.txt
@@ -48,6 +48,7 @@ set(osgXR_SRCS
     XRFramebuffer.cpp
     XRState.cpp
     XRRealizeOperation.cpp
+    XRUpdateOperation.cpp
     Action.cpp
     ActionSet.cpp
     CompositionLayer.cpp

src/XRState.cpp
@@ -382,7 +382,8 @@ void XRState::SceneViewAppView::addSlave(osg::Camera *slaveCamera)
     if (visMaskTransform.valid())
     {
         osg::View::Slave *slave = _osgView->findSlaveForCamera(slaveCamera);
-        slave->_updateSlaveCallback = new SceneViewUpdateSlaveCallback(_state, visMaskTransform.get());
+        if (slave)
+            slave->_updateSlaveCallback = new SceneViewUpdateSlaveCallback(_state, visMaskTransform.get());
     }
 }
 
@@ -611,8 +612,6 @@ bool XRState::checkAndResetStateChanged()
 
 void XRState::update()
 {
-    assert(_manager.valid());
-
     typedef UpResult (XRState::*UpHandler)();
     static UpHandler upStateHandlers[VRSTATE_MAX - 1] = {
         &XRState::upInstance,

src/XRUpdateOperation.cpp
@@ -1,0 +1,19 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#include "XRUpdateOperation.h"
+
+#include "XRState.h"
+
+using namespace osgXR;
+
+XRUpdateOperation::XRUpdateOperation(osg::ref_ptr<XRState> state) :
+    osg::Operation("XRUpdateOperation", true),
+    _state(state)
+{
+}
+
+void XRUpdateOperation::operator () (osg::Object *obj)
+{
+    _state->update();
+}

src/XRUpdateOperation.h
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2022 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_XRUPDATEOPERATION
+#define OSGXR_XRUPDATEOPERATION 1
+
+#include <osg/ref_ptr>
+#include <osg/OperationThread>
+
+namespace osgXR {
+
+class XRState;
+
+class XRUpdateOperation : public osg::Operation
+{
+    public:
+
+        explicit XRUpdateOperation(osg::ref_ptr<XRState> state);
+
+        void operator () (osg::Object *ojb) override;
+
+    protected:
+
+        osg::ref_ptr<XRState> _state;
+};
+
+} // osgXR
+
+#endif

commit eaefe91d8c4d805890c6a4af2c0c738f9acb37ee

OpenXRDisplay: Fix autoenable with Manager
Commit 75840ac1727e ("OpenXRDisplay: Add an update operation") set the
destination state to VRSTATE_ACTIONS from the XRRealizeOperation,
however this is also used by the Manager class even before VR gets
enabled. Move this code into OpenXRDisplay instead, so that users of the
Manager class don't get VR permanently defaulting to enabled.

Fixes: 75840ac1727e ("OpenXRDisplay: Add an update operation")
Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXRDisplay.cpp
@@ -40,6 +40,7 @@ void OpenXRDisplay::configure(osgViewer::View &view) const
 
     _state = new XRState(_settings);
     _state->setViewer(viewer);
+    _state->setDestState(XRState::VRSTATE_ACTIONS);
     viewer->setRealizeOperation(new XRRealizeOperation(_state, &view));
     viewer->addUpdateOperation(new XRUpdateOperation(_state));
 }

src/XRRealizeOperation.cpp
@@ -29,7 +29,6 @@ void XRRealizeOperation::operator () (osg::GraphicsContext *gc)
         if (window)
         {
             _state->init(window, _view);
-            _state->setDestState(XRState::VRSTATE_ACTIONS);
             _realized = true;
         }
     }