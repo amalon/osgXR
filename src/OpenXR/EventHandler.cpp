amalon/osgXR

commit c2f5dcd950c07994bac9f9061663a79dde3b852f

Separate event handling into callback class
Create an EventHandler class with virtual callbacks for the mid level
API to use to respond to OpenXR events.

XRState now derives from EventHandler and uses it to handle events
instead of the minimal handling that was in the Instance and Session
classes. No callbacks are overridden yet.

Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXR/EventHandler.cpp
@@ -1,0 +1,165 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#include "EventHandler.h"
+#include "Instance.h"
+#include "Session.h"
+
+#include <osg/Notify>
+
+using namespace osgXR;
+using namespace OpenXR;
+
+void EventHandler::onEvent(Instance *instance,
+                           const XrEventDataBuffer *event)
+{
+    switch (event->type)
+    {
+    case XR_TYPE_EVENT_DATA_EVENTS_LOST:
+        onEventsLost(instance,
+            reinterpret_cast<const XrEventDataEventsLost *>(event));
+        break;
+    case XR_TYPE_EVENT_DATA_INSTANCE_LOSS_PENDING:
+        onInstanceLossPending(instance,
+            reinterpret_cast<const XrEventDataInstanceLossPending *>(event));
+        break;
+    case XR_TYPE_EVENT_DATA_INTERACTION_PROFILE_CHANGED:
+        {
+            auto *profileEvent = reinterpret_cast<const XrEventDataInteractionProfileChanged *>(event);
+            Session *session = instance->getSession(profileEvent->session);
+            if (session)
+                onInteractionProfileChanged(session, profileEvent);
+            else
+                OSG_WARN << "Unhandled OpenXR interaction profile changed event: Session not registered" << std::endl;
+            break;
+        }
+    case XR_TYPE_EVENT_DATA_REFERENCE_SPACE_CHANGE_PENDING:
+        {
+            auto *spaceEvent = reinterpret_cast<const XrEventDataReferenceSpaceChangePending *>(event);
+            Session *session = instance->getSession(spaceEvent->session);
+            if (session)
+                onReferenceSpaceChangePending(session, spaceEvent);
+            else
+                OSG_WARN << "Unhandled OpenXR reference space change pending event: Session not registered" << std::endl;
+            break;
+        }
+    case XR_TYPE_EVENT_DATA_SESSION_STATE_CHANGED:
+        {
+            auto *stateEvent = reinterpret_cast<const XrEventDataSessionStateChanged *>(event);
+            Session *session = instance->getSession(stateEvent->session);
+            if (session)
+                onSessionStateChanged(session, stateEvent);
+            else
+                OSG_WARN << "Unhandled OpenXR session state change event: Session not registered" << std::endl;
+            break;
+        }
+    default:
+        onUnhandledEvent(instance, event);
+        break;
+    }
+}
+
+void EventHandler::onUnhandledEvent(Instance *instance,
+                                    const XrEventDataBuffer *event)
+{
+    OSG_WARN << "Unhandled OpenXR Event: " << event->type << std::endl;
+}
+
+void EventHandler::onEventsLost(Instance *instance,
+                                const XrEventDataEventsLost *event)
+{
+    OSG_WARN << event->lostEventCount << " OpenXR events lost" << std::endl;
+}
+
+void EventHandler::onInstanceLossPending(Instance *instance,
+                                         const XrEventDataInstanceLossPending *event)
+{
+    OSG_WARN << "OpenXR instance loss pending" << std::endl;
+}
+
+void EventHandler::onInteractionProfileChanged(Session *session,
+                                               const XrEventDataInteractionProfileChanged *event)
+{
+    OSG_WARN << "OpenXR interaction profile changed" << std::endl;
+}
+
+void EventHandler::onReferenceSpaceChangePending(Session *session,
+                                                 const XrEventDataReferenceSpaceChangePending *event)
+{
+    OSG_WARN << "OpenXR reference space change penging" << std::endl;
+}
+
+void EventHandler::onSessionStateChanged(Session *session,
+                                         const XrEventDataSessionStateChanged *event)
+{
+    XrSessionState oldState = session->getState();
+    session->setState(event->state);
+    switch (event->state)
+    {
+    case XR_SESSION_STATE_IDLE:
+        // Either starting or soon to be stopping
+        if (oldState == XR_SESSION_STATE_UNKNOWN)
+            onSessionStateStart(session);
+        break;
+    case XR_SESSION_STATE_READY:
+        // Session ready to begin
+        onSessionStateReady(session);
+        break;
+    case XR_SESSION_STATE_SYNCHRONIZED:
+        // Either session synchronised or no longer visible
+        break;
+    case XR_SESSION_STATE_VISIBLE:
+        // Either session now visible or lost focus
+        if (oldState == XR_SESSION_STATE_FOCUSED)
+            onSessionStateUnfocus(session);
+        break;
+    case XR_SESSION_STATE_FOCUSED:
+        // Session visible and in focus
+        onSessionStateFocus(session);
+        break;
+    case XR_SESSION_STATE_STOPPING:
+        // Session now stopping
+        onSessionStateStopping(session, false);
+        break;
+    case XR_SESSION_STATE_LOSS_PENDING:
+        // Session loss is pending, which can happen at any time
+        if (oldState == XR_SESSION_STATE_FOCUSED)
+            onSessionStateUnfocus(session);
+        if (session->isRunning())
+            onSessionStateStopping(session, true);
+        // Attempt restart
+        onSessionStateEnd(session, true);
+        break;
+    case XR_SESSION_STATE_EXITING:
+        // Session is exiting and should be cleaned up
+        onSessionStateEnd(session, false);
+        break;
+    default:
+        OSG_WARN << "Unknown OpenXR session state: " << event->state << std::endl;
+        break;
+    }
+}
+
+void EventHandler::onSessionStateStart(Session *session)
+{
+}
+
+void EventHandler::onSessionStateEnd(Session *session, bool retry)
+{
+}
+
+void EventHandler::onSessionStateReady(Session *session)
+{
+}
+
+void EventHandler::onSessionStateStopping(Session *session, bool loss)
+{
+}
+
+void EventHandler::onSessionStateFocus(Session *session)
+{
+}
+
+void EventHandler::onSessionStateUnfocus(Session *session)
+{
+}

src/CMakeLists.txt
@@ -17,6 +17,7 @@ set(osgXR_HEADERS
 # Source files
 set(osgXR_SRCS
     OpenXR/Compositor.cpp
+    OpenXR/EventHandler.cpp
     OpenXR/GraphicsBinding.cpp
     OpenXR/Instance.cpp
     OpenXR/Session.cpp

src/OpenXR/EventHandler.h
@@ -1,0 +1,72 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (C) 2021 James Hogan <james@albanarts.com>
+
+#ifndef OSGXR_OPENXR_EVENT_HANDLER
+#define OSGXR_OPENXR_EVENT_HANDLER 1
+
+#include <osg/Referenced>
+
+#include <openxr/openxr.h>
+
+namespace osgXR {
+
+namespace OpenXR {
+
+class Instance;
+class Session;
+
+/// This class handles OpenXR events.
+class EventHandler : public osg::Referenced
+{
+    public:
+
+        // Instance events
+
+        /// Top level OpenXR event handler.
+        void onEvent(Instance *instance, const XrEventDataBuffer *event);
+        /// Handle an otherwise unhandled event.
+        virtual void onUnhandledEvent(Instance *instance,
+                                      const XrEventDataBuffer *event);
+
+        /// Handle an events lost event.
+        virtual void onEventsLost(Instance *instance,
+                                  const XrEventDataEventsLost *event);
+        /// Handle an instance loss pending event.
+        virtual void onInstanceLossPending(Instance *instance,
+                                           const XrEventDataInstanceLossPending *event);
+
+        // Session events
+
+        /// Handle an interaction profile changed event.
+        virtual void onInteractionProfileChanged(Session *session,
+                                                 const XrEventDataInteractionProfileChanged *event);
+        /// Handle a reference space change pending event.
+        virtual void onReferenceSpaceChangePending(Session *session,
+                                                   const XrEventDataReferenceSpaceChangePending *event);
+        /// Handle a session state change event.
+        virtual void onSessionStateChanged(Session *session,
+                                           const XrEventDataSessionStateChanged *event);
+
+        // Session state events
+
+        /// Transition into initial idle state (idle, after init).
+        virtual void onSessionStateStart(Session *session);
+        /// Transition into ending state (exiting / loss pending, before cleanup).
+        virtual void onSessionStateEnd(Session *session, bool retry);
+
+        /// Transition into a ready state.
+        virtual void onSessionStateReady(Session *session);
+        /// Transition out of running state (stopping, before end).
+        virtual void onSessionStateStopping(Session *session, bool loss);
+
+        /// Transition into focused session state.
+        virtual void onSessionStateFocus(Session *session);
+        /// Transition out of focused session state.
+        virtual void onSessionStateUnfocus(Session *session);
+};
+
+} // osgXR::OpenXR
+
+} // osgXR
+
+#endif

src/OpenXR/Instance.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1-only
 // Copyright (C) 2021 James Hogan <james@albanarts.com>
 
+#include "EventHandler.h"
 #include "Instance.h"
 #include "Session.h"
 #include "System.h"
@@ -310,26 +311,15 @@ void Instance::unregisterSession(Session *session)
     _sessions.erase(session->getXrSession());
 }
 
-void Instance::handleEvent(const XrEventDataBuffer &event)
+Session *Instance::getSession(XrSession xrSession)
 {
-    switch (event.type)
-    {
-    case XR_TYPE_EVENT_DATA_SESSION_STATE_CHANGED:
-        {
-            auto *stateEvent = reinterpret_cast<const XrEventDataSessionStateChanged *>(&event);
-            auto it = _sessions.find(stateEvent->session);
-            if (it != _sessions.end())
-                (*it).second->handleEvent(*stateEvent);
-
-            break;
-        }
-    default:
-        OSG_WARN << "Unhandled OpenXR Event: " << event.type << std::endl;
-        break;
-    }
+    auto it = _sessions.find(xrSession);
+    if (it == _sessions.end())
+        return nullptr;
+    return (*it).second;
 }
 
-void Instance::handleEvents()
+void Instance::pollEvents(EventHandler *handler)
 {
     for (;;) {
         XrEventDataBuffer event;
@@ -342,6 +332,6 @@ void Instance::handleEvents()
         if (res == XR_EVENT_UNAVAILABLE)
             break;
 
-        handleEvent(event);
+        handler->onEvent(this, &event);
     }
 }

src/OpenXR/Instance.h
@@ -20,6 +20,7 @@ namespace osgXR {
 
 namespace OpenXR {
 
+class EventHandler;
 class System;
 class Session;
 
@@ -107,11 +108,11 @@ class Instance : public osg::Referenced
         System *getSystem(XrFormFactor formFactor);
         void registerSession(Session *session);
         void unregisterSession(Session *session);
+        Session *getSession(XrSession xrSession);
 
         // Events
 
-        void handleEvent(const XrEventDataBuffer &event);
-        void handleEvents();
+        void pollEvents(EventHandler *handler);
 
     protected:
 

src/OpenXR/Session.cpp
@@ -22,8 +22,6 @@ Session::Session(System *system,
     _viewConfiguration(nullptr),
     _state(XR_SESSION_STATE_UNKNOWN),
     _running(false),
-    _shouldEnd(false),
-    _shouldDestroy(false),
     _readSwapchainFormats(false),
     _createdLocalSpace(false),
     _localSpace(XR_NULL_HANDLE)
@@ -110,28 +108,6 @@ XrSpace Session::getLocalSpace() const
     }
 
     return _localSpace;
-}
-
-void Session::handleEvent(const XrEventDataSessionStateChanged &event)
-{
-    _state = event.state;
-    switch (_state)
-    {
-    case XR_SESSION_STATE_READY:
-        OSG_WARN << "OpenXR session ready" << std::endl;
-        break;
-    case XR_SESSION_STATE_EXITING:
-    case XR_SESSION_STATE_LOSS_PENDING:
-        _shouldDestroy = true;
-        OSG_WARN << "OpenXR session should be destroyed" << std::endl;
-        break;
-    case XR_SESSION_STATE_STOPPING:
-        _shouldEnd = true;
-        OSG_WARN << "OpenXR session should end" << std::endl;
-        break;
-    default:
-        break;
-    }
 }
 
 void Session::makeCurrent() const

src/OpenXR/Session.h
@@ -54,6 +54,19 @@ class Session : public osg::Referenced
             return _window.get();
         }
 
+        // State management
+
+        inline XrSessionState getState() const
+        {
+            return _state;
+        }
+
+        inline void setState(XrSessionState state)
+        {
+            _state = state;
+        }
+
+
         // Conversions
 
         inline const osg::ref_ptr<Instance> getInstance() const
@@ -87,10 +100,6 @@ class Session : public osg::Referenced
         const SwapchainFormats &getSwapchainFormats() const;
 
         XrSpace getLocalSpace() const;
-
-        // Events
-        
-        void handleEvent(const XrEventDataSessionStateChanged &event);
 
         // Operations
 
@@ -240,8 +249,6 @@ class Session : public osg::Referenced
         // Session state
         XrSessionState _state;
         bool _running;
-        bool _shouldEnd;
-        bool _shouldDestroy;
 
         // Swapchain formats
         mutable bool _readSwapchainFormats;

src/XRState.cpp
@@ -668,8 +668,8 @@ osg::ref_ptr<OpenXR::Session::Frame> XRState::getFrame(osg::FrameStamp *stamp)
     if (frame.valid())
         return frame;
 
-    // There's probably a better place to handle events
-    _instance->handleEvents();
+    // Poll for events
+    _instance->pollEvents(this);
     if (!_session->isRunning())
     {
         if (!_session->isReady())

commit ec2b977e904c963be7beea467547f9c959c52015

OpenXR/EventHandler: Fix typo in warning message
Fix typo in warning message for onReferenceSpaceChangePending(),
s/penging/pending/.

Fixes: c2f5dcd950c0 ("Separate event handling into callback class")
Signed-off-by: James Hogan <james@albanarts.com>

src/OpenXR/EventHandler.cpp
@@ -96,7 +96,7 @@ void EventHandler::onInteractionProfileChanged(Session *session,
 void EventHandler::onReferenceSpaceChangePending(Session *session,
                                                  const XrEventDataReferenceSpaceChangePending *event)
 {
-    OSG_WARN << "OpenXR reference space change penging" << std::endl;
+    OSG_WARN << "OpenXR reference space change pending" << std::endl;
 }
 
 void EventHandler::onVisibilityMaskChanged(Session *session,